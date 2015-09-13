c56ddd9c4564433e14a3ddb9078d6c6fb6dab033
stevene8seven <scuba.7.steve@gmail.com>
2015-09-13 01:50:43 -0500

    coded js. functionality not complete

---
+++ js/application.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+	// Validate text box, then add value in text box to list
+
+	$('#add').click(function() {
+		if( $('#input_text').val().trim().length == 0 ) {
+				// alert("missing");
+				$('#error').show();
+				$('#input_text').val("");
+		} else { 
+
+		$('.items').append('<li class="item"><button class="remove">Mark Complete</button>' + $('#input_text').val() + '</li>');
+		$('#input_text').val("");
+		$('#error').hide();
+
+		};	
+	
+	});
+
+	// Change class when user clicks Mark Complete button	
+
+	$('.items').on('click', '.new_item', function() {
+		$(this).closest('li').toggleClass('new_item complete');
+		$(this).remove();
+
+	});
+
+});
